48f80df1ae07d7172dad37f8b132f067301ba389
Wosser1sProductions <wosser1@hotmail.com>
2015-12-14 14:36:59 +0100

    Reformat.

--- main.cpp
+++ main.cpp
@@ -9,13 +9,15 @@ int main() {
 
 	Topology n;
 
-
 	n.addDevice("PC 1", DeviceType::CLIENT);
 	n.addDevice("HUB", DeviceType::HUB);
 	n.addDevice("Server", DeviceType::SERVER);
 
-	for (NetwerkApparaat *o : n.getDeviceList())
+	for (NetwerkApparaat *o : n.getDeviceList()) {
 		cout << "Network device: " << o->getName() << endl;
+		cout << "     " << o->request().getData() << endl;
+		cout << "       " << o->reply().getData() << endl;
+	}
 
 	return 0;
 }
